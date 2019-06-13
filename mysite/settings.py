1261a3e5ffdf3fc06df349a77e0515a8c3dfe18b
Javier Tobar <ja.tobarv@alumnos.duoc.cl>
2019-06-13 13:09:50 -0400

    Agrega foro y post

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'marketplace.apps.MarketplaceConfig'
+    'marketplace.apps.MarketplaceConfig',
+    'foro'
 ]
 
 MIDDLEWARE = [
